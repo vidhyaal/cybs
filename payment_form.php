1a7fb5e5f2efaf38a836143dbe19bf0dd8f318d0
vidhyaal <vidhya_al@yahoo.com>
2021-07-26 03:06:25 +0800

    .

--- payment_form.php
+++ payment_form.php
@@ -15,8 +15,10 @@
 </head>
 <body>
 <form name="shopping" method="post" id="shopping" action="payment_confirmation.php">
-             <input type="hidden" name="access_key" value="23879b2b57323605932e40d2884de6d2">
-              <input type="hidden" name="profile_id" value="488DF726-70E4-4690-8627-FAF66FB44E18">
+		<input type="hidden" name="access_key" value="1d3e1a35602e3e149d06de9c1a76bbe8">
+    <input type="hidden" name="profile_id" value="0003C773-CA4D-47A7-B98D-47C1526006EB">
+             <!--input type="hidden" name="access_key" value="23879b2b57323605932e40d2884de6d2">
+              <input type="hidden" name="profile_id" value="488DF726-70E4-4690-8627-FAF66FB44E18"-->
               <input type="hidden" name="transaction_uuid" value="<?php echo uniqid() ?>">
               <input type="hidden" name="signed_field_names" value="access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency">
               <input type="hidden" name="unsigned_field_names" value="bill_to_forename,bill_to_surname,bill_to_address_line1,bill_to_address_city,bill_to_address_state,bill_to_address_postal_code,bill_to_address_country,bill_to_email"/>
